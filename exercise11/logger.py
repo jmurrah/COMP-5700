c85cf98017c8e01cc9bcd676addd750dcd315fde
jmurrah <jacobhmurrah@gmail.com>
2025-10-30 10:13:36 -0500

    exercise11

--- exercise11/logger.py
+++ exercise11/logger.py
@@ -1,47 +1,37 @@
+"""
+COMP-5700 Exercise 11: Forensics with Logging (logger)
+Author: Jacob Murrah
+Date: 11/03/2025
+"""
+
+
 import logging
+from pathlib import Path
+import time
+
+_LOG_FILE = Path(__file__).with_name("OUTPUT.log")
+_LOG_FORMAT = "%(asctime)s %(levelname)s %(funcName)s %(message)s"
+_DATE_FORMAT = "%Y-%m-%d %H:%M:%S %Z%z"
+
+logger = logging.getLogger("exercise11")
+logger.setLevel(logging.INFO)
+logger.propagate = False
+
+if not logger.handlers:
+    file_handler = logging.FileHandler(_LOG_FILE, mode="a", encoding="utf-8")
+    formatter = logging.Formatter(_LOG_FORMAT, _DATE_FORMAT)
+
+    formatter.converter = time.localtime
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
 
-class ParamLogger(logging.Logger):
-    def _log(self, level, msg, args, **kwargs):
-        extra = kwargs.pop("extra", {})
-        extra.update(kwargs)
-        super()._log(
-            level,
-            msg,
-            args,
-            exc_info=kwargs.get("exc_info"),
-            extra=extra,
-            stack_info=kwargs.get("stack_info", False),
-        )
+def quote_str(val):
+    if isinstance(val, str):
+        return f"'{val}'"
+    return val
 
 
-logging.setLoggerClass(ParamLogger)
-
-logging.basicConfig(
-    level=logging.INFO,
-    format=(
-        "%(asctime)s [%(levelname)s] "
-        "[User=%(user)s | Realm=%(realm)s] "
-        "[System=%(system)s | Component=%(component)s] "
-        "[Status=%(status)s] "
-        "[Context=%(context)s] "
-        "[From=%(source)s] "
-        "%(message)s"
-    ),
-    datefmt="%Y-%m-%d %H:%M:%S %Z",
-    handlers=[logging.StreamHandler()],
-)
-
-logger = logging.getLogger(__name__)
-
-# EXAMPLE USAGE:
-# logger.info(
-#     "Loading dataset from data/train.csv",
-#     user="dolly",
-#     realm="AuburnSSO",
-#     system="MLPipeline",
-#     component="DataLoader",
-#     status="IN_PROGRESS",
-#     context="DatasetRead",
-#     source="filesystem"
-# )
+def get_logger() -> logging.Logger:
+    logger.info("Starting log for the exercise 11 code snippet.")
+    return logger
--- exercise11/snippet.py
+++ exercise11/snippet.py
@@ -1,11 +1,65 @@
+"""
+COMP-5700 Exercise 11: Forensics with Logging (snippet)
+Author: Jacob Murrah
+Date: 11/03/2025
+"""
+
+
+from logger import get_logger, quote_str
+
+logger = get_logger()
+
+
 def simpleDiv(a, b):
-    return a/b 
+    logger.info("Arguments provided -> a:%s, b:%s", quote_str(a), quote_str(b))
+    try:
+        return a / b
+    except Exception:
+        logger.exception(
+            "Exception encountered with a=%s, b=%s", quote_str(a), quote_str(b)
+        )
+
 
 def mul(a, b):
-    return a*b 
+    logger.info("Arguments provided -> a:%s, b:%s", quote_str(a), quote_str(b))
+    try:
+        return a * b
+    except Exception:
+        logger.exception(
+            "Exception encountered with a=%s, b=%s", quote_str(a), quote_str(b)
+        )
+
 
 def add(a, b):
-    return a + b 
+    logger.info("Arguments provided -> a:%s, b:%s", quote_str(a), quote_str(b))
+    try:
+        return a + b
+    except Exception:
+        logger.exception(
+            "Exception encountered with a=%s, b=%s", quote_str(a), quote_str(b)
+        )
+
 
 def sub(a, b):
-    return a - b 
+    logger.info("Arguments provided -> a:%s, b:%s", quote_str(a), quote_str(b))
+    try:
+        return a - b
+    except Exception:
+        logger.exception(
+            "Exception encountered with a=%s, b=%s", quote_str(a), quote_str(b)
+        )
+
+
+if __name__ == "__main__":
+    print("Running code snippet with logging...")
+    simpleDiv(10, 2)  # happy path
+    simpleDiv(10, "4")  # type error exception
+
+    mul(5, 4)  # happy path
+    mul("5", 4)  # type error exception
+
+    add(3, 7)  # happy path
+    add("3", 7)  # type error exception
+
+    sub(10, 4)  # happy path
+    sub(10, "4")  # type error exception
